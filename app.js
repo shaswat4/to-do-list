41d01c3c6658f03cc7a3fc42f99e3a3de951ae97
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-19 19:40:35 +0530

    add ejs render engine and connect it to lists.ejs

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
+
 
 app.get( '/', function (req , res){
-    res.send("hello");
+
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let day = weekday[d.getDay()];
+
+    res.render( 'lists' , {day_var : day } );
+
 });
 
 app.listen(3000 , function (){
