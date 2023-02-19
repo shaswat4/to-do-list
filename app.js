28e55be76210f2d1ee40ddcc4b4a8d235e359c9e
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-19 22:15:18 +0530

    changed day formatiing

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ app.set('view engine', 'ejs');
 
 app.get( '/', function (req , res){
 
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const d = new Date();
-    let day = weekday[d.getDay()];
-
+    var day = getDay();
+    
     res.render( 'list' , {day_var : day } );
 
 });
@@ -19,3 +17,22 @@ app.get( '/', function (req , res){
 app.listen(3000 , function (){
     console.log("listening at port 3000");
 })
+
+
+function getDay() {
+    
+    //const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    //let day = weekday[d.getDay()];
+
+    var options = {
+        weekday : "long",
+        day : "numeric" , 
+        month : "long"
+    };
+
+    var temp = d.toLocaleDateString("en-US" , options);
+    console.log(temp);
+
+    return temp;
+}
